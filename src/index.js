f24ea0edf3f20f9a9973ba48f53eef81d16302a1
Francisco González <Paco23456@outlook.com>
2025-01-06 15:31:04 -0600

    email_to

--- src/index.js
+++ src/index.js
@@ -10,6 +10,38 @@
 
 export default {
 	async fetch(request, env, ctx) {
+		if (request.method === 'POST') {
+			const data = await request.json();
+			const { name, lastname, email, subject, interest, company, projectDescription, message } = data.form;
+
+			const SENDGRID_API_KEY = env.SENDGRID_API_KEY;
+
+
+			const response = await fetch('https://api.sendgrid.com/v3/mail/send', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${SENDGRID_API_KEY}`,
+				},
+				body: JSON.stringify({
+					personalizations: [{
+						to: [{ email: env.EMAIL_TO }],
+						subject: subject,
+					}],
+					from: { email: email },
+					content: [{
+						type: 'text/plain',
+						value: `Name: ${name} ${lastname}\nCompany: ${company}\nInterest: ${interest}\nProject Description: ${projectDescription}\nMessage: ${message}`,
+					}],
+				}),
+			});
+
+			if (!response.ok) {
+				return new Response('Failed to send email', { status: 500 });
+			}
+
+			return new Response('Email sent successfully');
+		}
 		return new Response('Hello World!');
 	},
 };
